578e1e8aa420144129332662daa0e405919ff4c3
Yudha13Koley <yudhajit1998@gmail.com>
2020-09-21 11:09:35 +0530

    done

---
+++ src/main/java/samples/JavaHello/App.java
@@ -0,0 +1,22 @@
+package samples.JavaHello;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{public static final Logger LOG = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	String message = "Hello, World";
+    	LOG.debug(message+"Will be printed on Debug");
+    	LOG.info(message+"Will be printed on Info");
+    	LOG.warn(message+"Will be printed on Warn");
+    	LOG.error(message+"Will be printed on Error");
+    	LOG.fatal(message+"Will be printed on Fatal");
+    	LOG.info("Appending String : {}.",message);
+    	System.out.println(message);
+    }
+}
